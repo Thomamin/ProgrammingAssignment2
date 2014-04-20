a63b28960d20e072262be2edf9b8880b492ad720
Thomamin <thomamin@hotmail.com>
2014-04-20 11:30:02 +0900

    Initial working version
    The first working code as assignment requests

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code in R has 2 functions to handle repetitive inverse calculation of 
+## a matrix efficiently using cache
 
-## Write a short comment describing this function
-
+## Inverse a matrix and store the inversed one in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- matrix(,0,0)
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinver <- function(solve) m <<- solve
+    getinver <- function() m
+    list(set = set, get = get,
+         setinver = setinver,
+         getinver = getinver)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinver()
+    #check if there was a cached matrix. 
+    #Since the assignment says that all input matrices are inversable, 
+    #checking whether the cached matrix is 0x0 is enough
+    if(!(ncol(m)==0)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinver(m)
+    m
 }
